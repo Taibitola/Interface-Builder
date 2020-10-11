78a824e581cdd7b03db54947b5fd0df3a40d47b3
Tito Aderemi-Ibitola <taibitola@gmail.com>
2020-10-10 22:55:01 -0400

    Quick App "Hello" User

--- Interface Builder/ViewController.swift
+++ Interface Builder/ViewController.swift
@@ -8,13 +8,18 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    @IBOutlet weak var textField: NSTextField!
+    
+    @IBOutlet weak var label: NSTextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func buttonwaspressed(_ sender: Any) {
+        label.stringValue = "Hello \(textField.stringValue)!"
+    }
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
